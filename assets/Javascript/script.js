f8cba3faad0771175359bc06bd60a3072b257334
Ike Ekwueme <cryptoekwu@gmail.com>
2020-02-04 21:00:56 -0600

    Initial files

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,3 +1,12 @@
 var searchWeather = function(city){
     var queryURL = 'api.openweathermap.org/data/2.5/weather?q='+ city +' &APPID=f0a6c63cca981217d4cbd25627324a7b'
-}
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response){
+        console.log(response)
+    });
+};
+
+console.log(searchWeather('London'))
