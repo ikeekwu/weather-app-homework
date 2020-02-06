ee6e51d3947f0d934b950bfa0046b4cee3d9f195
Ike Ekwueme <cryptoekwu@gmail.com>
2020-02-05 23:44:32 -0600

    couldn't get ajax to load days

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,12 +1,17 @@
-var searchWeather = function(city){
-    var queryURL = 'api.openweathermap.org/data/2.5/weather?q='+ city +' &APPID=f0a6c63cca981217d4cbd25627324a7b'
 
+var weatherImg = document.querySelector('#weatherImg');
+var cityName = document.querySelector('#city-name');
+var tempF = document.querySelector('#tempF');
+var submit = document.querySelector('#submitWeather')
+var searchInput = document.querySelector('#searchInput').value
+
+var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q='+searchInput+'&APPID=f0a6c63cca981217d4cbd25627324a7b'
+
+submit.click(function(){
     $.ajax({
         url: queryURL,
         method: 'GET'
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response)
-    });
-};
-
-console.log(searchWeather('London'))
+    })
+});
